55cbcec7417f8e9d94379ce84908e2c2222287c0
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-22 03:19:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,9 +2,9 @@ $phrase = "It's-a me, Mario!"
 
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts $phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
